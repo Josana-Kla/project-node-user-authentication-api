b768ab6fbb3f4c894d0a35033466cdcbe97ebc2e
JosanaKla <josana.0205@gmail.com>
2022-01-19 17:21:28 -0300

    developing and testing the post method

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,18 @@ import usersRoute from './routes/users.route';
 
 const app = express();
 
+// Application Settings - Global Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes Settings
 app.use(usersRoute);
 
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
   res.status(200).send({ foo: 'Success!'});
 });
 
+// Server Startup
 app.listen(3000, () => {
   console.log('The application is running on port 3000!')
 });
