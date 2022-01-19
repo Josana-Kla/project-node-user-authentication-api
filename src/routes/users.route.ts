3a723e09707e5e624fa70451091283f4aceca0a2
JosanaKla <josana.0205@gmail.com>
2022-01-19 16:32:57 -0300

    status code enhancement with rest pattern

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -5,17 +5,18 @@
 //delete / users / :uuid
 
 import { Request, Response, NextFunction, Router } from "express";
+import { StatusCodes } from 'http-status-codes';
 
 const usersRoute = Router(); 
 
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
   const users = [{ userName: 'John'}];
-  res.status(200).send(users);
+  res.status(StatusCodes.OK).send(users);
 });
 
 usersRoute.get('users/:uuid', (req: Request <{ uuid: string }>, res: Response, next: NextFunction) => {
   const uuid = req.params.uuid;
-  res.status(200).send({ uuid });
+  res.status(StatusCodes.OK).send({ uuid });
 });
 
 export default usersRoute;
