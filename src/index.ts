d26cdef55528a3c4518d260fb52a5333e3c02ab0
JosanaKla <josana.0205@gmail.com>
2022-01-19 18:12:40 -0300

    refactoring the status route

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, NextFunction } from 'express';
 import usersRoute from './routes/users.route';
+import statusRoute from './routes/status.route';
 
 const app = express();
 
@@ -8,12 +9,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes Settings
+app.use(statusRoute);
 app.use(usersRoute);
-
-
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send({ foo: 'Success!'});
-});
 
 // Server Startup
 app.listen(3000, () => {
