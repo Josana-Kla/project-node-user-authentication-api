87326b757e733994a5433b3ef35ddc04ccda4c1d
JosanaKla <josana.0205@gmail.com>
2022-01-24 14:44:08 -0300

    building an Error Handling

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express';
 import usersRoute from './routes/users.route';
 import statusRoute from './routes/status.route';
+import errorHandler from './middlewares/error.handler.middleware';
 
 const app = express();
 
@@ -11,6 +12,9 @@ app.use(express.urlencoded({ extended: true }));
 // Routes Settings
 app.use(statusRoute);
 app.use(usersRoute);
+
+// Error Handler Configuration
+app.use(errorHandler);
 
 // Server Startup
 app.listen(3000, () => {
